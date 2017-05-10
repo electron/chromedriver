8ae6e4d458e8d4684932cb737c6bdc9297a186c9
Kevin Sawicki <kevinsawicki@gmail.com>
2017-05-10 14:43:18 -0700

    Prepare for Electron 1.7.0

--- test/chromedriver-test.js
+++ test/chromedriver-test.js
@@ -20,7 +20,7 @@ describe('chromedriver binary', function () {
     chromeDriver.stderr.on('data', function (data) { output += data })
 
     chromeDriver.on('close', function () {
-      assert.equal(output.indexOf('ChromeDriver 2.25'), 0)
+      assert.equal(output.indexOf('ChromeDriver 2.27'), 0, 'Unpexected version: ' + output)
     })
 
     chromeDriver.on('error', done)
