199c5f60eb1a92365343c5e587c9339462c427e3
Shelley Vohr <shelley.vohr@gmail.com>
2018-10-21 09:30:45 -0700

    feat: update to 3.0.0 stable

--- chromedriver.js
+++ chromedriver.js
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
 
-var ChildProcess = require('child_process')
-var path = require('path')
+const ChildProcess = require('child_process')
+const path = require('path')
 
-var command = path.join(__dirname, 'bin', 'chromedriver')
-var args = process.argv.slice(2)
-var options = {
+const command = path.join(__dirname, 'bin', 'chromedriver')
+const args = process.argv.slice(2)
+const options = {
   cwd: process.cwd(),
   env: process.env,
   stdio: 'inherit'
 }
 
-var chromeDriverProcess = ChildProcess.spawn(command, args, options)
+const chromeDriverProcess = ChildProcess.spawn(command, args, options)
 
-var killChromeDriver = function () {
+const killChromeDriver = () => {
   try {
     chromeDriverProcess.kill()
-  } catch (ignored) {
-  }
+  } catch (ignored) {}
 }
 
 process.on('exit', killChromeDriver)
