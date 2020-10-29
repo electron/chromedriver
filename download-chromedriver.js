9ff99d7eb11576a2d277772d8aa29a61505a509c
Ryan Anderson <anderson.ryan92@yahoo.com>
2020-10-28 22:18:16 -0700

    added log statement for debugging

--- download-chromedriver.js
+++ download-chromedriver.js
@@ -1,14 +1,26 @@
 const { promises: fs } = require('fs')
 const path = require('path')
+const { downloadArtifact } = require('@electron/get')
 const extractZip = require('extract-zip')
 const versionToDownload = require('./package').version
-const args = require('minimist')(process.argv.slice(2));
-const request = require('request');
+
+function download (version) {
+  return downloadArtifact({
+    version,
+    artifactName: 'chromedriver',
+    platform: process.env.npm_config_platform,
+    arch: process.env.npm_config_arch,
+    rejectUnauthorized: process.env.npm_config_strict_ssl === 'true',
+    quiet: ['info', 'verbose', 'silly', 'http'].indexOf(process.env.npm_config_loglevel) === -1
+  })
+}
 
 async function attemptDownload (version) {
   try {
     const targetFolder = path.join(__dirname, 'bin')
-    const zipPath = 'https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_mac64.zip'
+    const zipPath = await download(version)
+    console.log('zipPath is: ')
+    console.log(zipPath)
     await extractZip(zipPath, { dir: targetFolder })
     const platform = process.env.npm_config_platform || process.platform
     if (platform !== 'win32') {
@@ -16,7 +28,6 @@ async function attemptDownload (version) {
     }
   } catch (err) {
     // attempt to fall back to semver minor
-    console.log('got to catch block')
     const parts = version.split('.')
     const baseVersion = `${parts[0]}.${parts[1]}.0`
 
