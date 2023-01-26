e5c566a79917ad0800130e29eb7ed6eae3437ddd
Shelley Vohr <shelley.vohr@gmail.com>
2023-01-26 13:44:54 +0100

    chore: graceful exit on unsupported version

--- script/update-version.js
+++ script/update-version.js
@@ -10,8 +10,8 @@ const normalizeVersion = (version = '') => {
 async function updateVersion () {
   const version = normalizeVersion(process.argv[2])
   if (!versionFormat.test(version)) {
-    console.error(`Invalid version ${version}`)
-    process.exit(1)
+    console.error(`Unsupported version ${version} - only major, minor, and patch releases are currently supported`)
+    return;
   }
 
   const PJ_PATH = path.join(__dirname, '..', 'package.json')
